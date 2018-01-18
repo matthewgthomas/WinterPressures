6126e37eb27df5eadaec4da4c449f363290dc8d6
Matthew Gwynfryn Thomas <matthewgthomas@gmail.com>
2018-01-18 09:41:08 +0000

    Plot Independent Living services on the map

--- update map.r
+++ update map.r
@@ -6,8 +6,12 @@
 source("init.r")
 
 # import and process the most recent situation report
-# note: you'll need to update the `sitrep_filename` variable on line 14 of this file
 source("process sitrep.r")
+
+# import and process BRC's independent living services
+# note: you should only need to run this once (or after every extract)
+# source("process services.r")
 
 # produce map as .html file (all data and javascript will be contained in the single file)
 rmarkdown::render("winterpressures.Rmd", output_file="index.html", output_dir="output")
+rmarkdown::render("winterpressures-services.Rmd", output_file="index-services.html", output_dir="output")
